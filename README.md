dca08b073bf89a7bb2cb3dd9e2eb939fdaa00330
lovey <deepi29111998@gmail.com>
2018-08-04 02:02:05 +0530

    AngularJs

---
+++ README.md
@@ -0,0 +1,9 @@
+# AngularJS
+
+Run the following commands in terminal for AngularJs server :
+ 
+1. sudo npm install -g @angular/cli
+2. npm install
+3. ng serve --open
+4. localhost:4200 (open in firefox)
+
---
+++ src/assets/js/app.js
@@ -0,0 +1,30 @@
+var app = angular.module("MyApp", ["ui.bootstrap.modal"]);
+
+app.controller("MyCtrl", function($scope) {
+
+  $scope.open = function() {
+    $scope.showModal = true;
+  };
+
+  $scope.ok = function() {
+    $scope.showModal = false;
+  };
+
+  $scope.cancel = function() {
+    $scope.showModal = false;
+  };
+
+});
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    span.onclick = function() {
+    	modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
